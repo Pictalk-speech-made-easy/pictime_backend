2d9ae382f3ecbaafd5461227b78d8ca92ed2f4c9
Clément Baroudi <clement.baroudi@insa-lyon.fr>
2022-11-22 11:16:29 +0100

    test

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,6 +6,7 @@ import { AuthCredentialsDto } from './dto/AuthCredentialsDto';
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  
   @Post('/signup')
   signup(@Body() authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return this.authService.signUp(authCredentialsDto);
